6f332c8f93247de93bbe62a3f57baf40a3681f6f
Monica <mcrdz520@gmail.com>
2021-08-16 18:12:47 -0500

    ConterApp y Pruebas

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PrimeraApp from './PrimeraApp';
+//import PrimeraApp from './PrimeraApp';
+import CounterApp from './CounterApp'
 import './index.css';
 
 // const saludo = <h1>Hola</h1>;
 
 const divRoot = document.querySelector('#app');
 
-ReactDOM.render( <PrimeraApp saludo="Hola" />, divRoot );
+ReactDOM.render( <CounterApp value={ 5 } />, divRoot );
 
